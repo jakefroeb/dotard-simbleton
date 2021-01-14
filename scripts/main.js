3845e8567ecda1fe600f507eb92a836482a995a9
jakefroeb <jake.herman.froeb@gmail.com>
2021-01-14 13:45:45 -0600

    added the Agent.js and AgentList.js which convert the new mapped array into Html text and then insert them into the dom

--- scripts/main.js
+++ scripts/main.js
@@ -4,3 +4,5 @@ import {NewYorkBusinessList} from "./BusinessList.js"
 NewYorkBusinessList();
 import {ManufacturingBusinessList} from "./BusinessList.js"
 ManufacturingBusinessList();
+import {AgentList} from "./AgentList.js"
+AgentList();
